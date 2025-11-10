cb5949b50f1e7bb3e5a4df97f0fbb96abd0303da
Azhriler Lintang <azhrielazazel@gmail.com>
2025-11-10 11:30:57 +0700

    add login for mobile

---
+++ frontend/lib/features/splash/screen/screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToAuth();
+  }
+
+  void _navigateToAuth() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Get.offNamed('/auth');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Manajemen Karyawan',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
